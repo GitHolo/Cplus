e964752693274bbc12a6779fbd2dce926a862f19
GitHolo <ooye2332@gmail.com>
2022-02-28 15:03:24 +0100

    fajowe

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿// ConsoleApplication1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+    int liczba = 0;
+    int p = 2;
+    int n = 0;
+    cout << "ile liczb pierwszych mam wyswietlic?" << endl;
+    cin >> n;
+    while liczba <= n{
+
+    }
+}
+
