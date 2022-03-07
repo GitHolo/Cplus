7284e46bc61e2c255f39674eec59e38bfc3c7308
GitHolo <ooye2332@gmail.com>
2022-03-07 15:02:37 +0100

    ?

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,13 +6,25 @@ using namespace std;
 int main()
 {
     cout << "Hello World!\n";
-    int liczba = 0;
+    int l = 2;
     int p = 2;
     int n = 0;
     cout << "ile liczb pierwszych mam wyswietlic?" << endl;
     cin >> n;
-    while liczba <= n{
-
+    for(int b = 1; b<=n; b++)
+    {
+        for (int a = 1; a <= n; a++)
+        {
+            if (l % p == 0)
+            {
+                p = p + 1;
+            }
+            else
+            {
+                cout << l << endl;
+                l++;
+            }
+        }
     }
 }
 
