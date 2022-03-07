bf2e9fba4db469292803efbe7c04fe8b70a8c35a
GitHolo <ooye2332@gmail.com>
2022-03-07 17:52:08 +0100

    Dziala program :D

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,30 @@
+﻿// ConsoleApplication1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 0;
+    cout << "ile liczb pierwszych mam wyswietlic?" << endl;
+    cin >> n;
+    int p = 2;
+    for (int c = 0; c < n; c)
+    {
+        bool pier = true;
+        for (int i = 2; i <= p / 2; ++i) {
+            if (p % i == 0) {
+                pier = false;
+                break;
+            }
+        }
+        if (pier)
+        {
+            cout << p << endl;
+            c++;
+        }
+        p++;
+        
+    }
+}
+
