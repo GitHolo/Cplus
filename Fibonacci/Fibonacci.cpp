27533e85c7a81b1728f9e6518cc4bc7847007acf
GitHolo <ooye2332@gmail.com>
2022-04-11 15:02:58 +0200

    Fibonacci

---
+++ Fibonacci/Fibonacci.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    bool b;
+    int c = 0;
+    int i=0;
+    int d = 1;
+    int f=0;
+    do
+    {
+        cout << "podaj liczbę fibonacci do wypisania\n";
+        cin >> a;
+        if (a > 0)
+        {
+            b = true;
+        }
+        else
+        {
+            b = false;
+            cout << "blad\n";
+        }
+    } while (b == false);
+
+    for (i; i < a; i++)
+    {
+        cout << c << ", ";
+        f = c;
+        c = c + d;
+        d = f;
+    }
+}
--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Linki do zrealizowanych programów:
 1. [Liczby Pierwsze](https://github.com/GitHolo/Info/tree/main/LiczbyPierwsze)
 2. [NWW I NWD](https://github.com/GitHolo/Info/tree/main/NWWiNWD)
 3. [Szyfr Cezara](https://github.com/GitHolo/Info/tree/main/SzyfrCezara)
+4. [Kod Fibonacci](https://github.com/GitHolo/Info/tree/main/Fibonacii)
